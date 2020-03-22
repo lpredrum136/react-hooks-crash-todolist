11bbc1989796fd81e09b347e81b85ad34edeece4
Henry Nguyen <nguyenhai1591@gmail.com>
2020-03-22 21:43:58 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
+const Todo = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ textDecoration: props.todo.completed ? 'line-through' : '' }}
+      className='todo'
+    >
+      {props.todo.text}
+      <div>
+        <button onClick={props.markComplete.bind(this, props.index)}>
+          Complete
+        </button>
+        <button onClick={props.deleteTodo.bind(this, props.index)}>
+          Delete
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+const TodoForm = props => {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (!value) return;
+    props.addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='input'
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        placeholder='Add todo...'
+      />
+      <input type='submit' value='Submit' />
+    </form>
+  );
+};
+
+const App = props => {
+  const [todos, setTodos] = useState([
+    { text: 'Learn Hooks', completed: false },
+    { text: 'Meet friends for lunch', completed: false },
+    { text: 'Build cool Todo app', completed: false }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text: text, completed: false }];
+    setTodos(newTodos);
+  };
+
+  const markComplete = indexCompleted => {
+    /* CACH 1
+    const newTodos = todos.map((todo, index) => {
+      if (index === indexCompleted) todo.completed = !todo.completed;
+      return todo;
+    });*/
+
+    const newTodos = [...todos];
+    newTodos[indexCompleted].completed = !newTodos[indexCompleted].completed;
+
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = indexDelete => {
+    const newTodos = todos.filter((todo, index) => index !== indexDelete);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            markComplete={markComplete}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+};
+
+App.propTypes = {};
 
 export default App;
